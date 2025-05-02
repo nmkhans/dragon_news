9a06ab506d49566902977962c728b81e60afca96
nmkhans <nurmoin05@gmail.com>
2025-05-02 21:11:58 +0600

    project initialized

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import router from "./routes/router";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
